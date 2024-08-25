9475f43dd8bac5220fff3a81193dc66980fa0926
Sai Ashirwad <saiashirwadr@gmail.com>
2024-08-25 06:05:48 +0530

    slommy

---
+++ README.md
@@ -0,0 +1,4 @@
+# Slommy
+
+![Cats can have a little slommy](https://i.redd.it/0m715vv0ixzb1.jpeg)
+
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export function hello(): string {
+	return "hi";
+}
+
+export function bye(): string {
+	return "bye";
+}
